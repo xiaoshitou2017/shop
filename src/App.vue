175a2e07dd078d4181482938b9683fce1963da2e
yanglei <393388780@qq.com>
2019-03-28 16:13:04 +0800

    init shop

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <ul class="footer-bar">
+      <li>首页</li>
+      <li>分类</li>
+      <li>购物车</li>
+      <li>我的</li>
+    </ul>
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
